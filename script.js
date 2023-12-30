f52bc3b047b4b4c6e378fdf4c11fb6aa4663f93b
CodeRacon <michael.buschmann.86@gmail.com>
2023-12-30 16:06:06 +0100

    successfully fetched data

--- script.js
+++ script.js
@@ -3,5 +3,17 @@ async function getSynonyms() {
     'https://www.openthesaurus.de/synonyme/search?q=test&format=application/json';
 
   let response = await fetch(url);
-  console.log('Response is: ', response);
+  let responseAsJSON = await response.json();
+  let synsets = responseAsJSON['synsets'];
+  renderSynsets(synsets);
+}
+
+function renderSynsets(synsets) {
+  let container = document.getElementById('container');
+
+  container.innerHTML = /*html*/ `
+    <div>
+      Es wurden <b> ${synsets.length} </b>Synonym-Sets geladen.
+    </div>
+  `;
 }
