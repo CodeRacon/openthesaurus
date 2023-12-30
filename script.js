ecd7dfd1bf931151027ea2145613e560ea944d62
CodeRacon <michael.buschmann.86@gmail.com>
2023-12-30 15:28:55 +0100

    coded test-function to fetch data

---
+++ script.js
@@ -0,0 +1,7 @@
+async function getSynonyms() {
+  let url =
+    'https://www.openthesaurus.de/synonyme/search?q=test&format=application/json';
+
+  let response = await fetch(url);
+  console.log('Response is: ', response);
+}
